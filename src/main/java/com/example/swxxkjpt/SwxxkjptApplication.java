d7cba87d670793508f45545d59075bc36041b908
ylm <1259854353@qq.com>
2025-12-02 15:02:39 +0800

    first test

---
+++ src/main/java/com/example/swxxkjpt/SwxxkjptApplication.java
@@ -0,0 +1,13 @@
+package com.example.swxxkjpt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwxxkjptApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SwxxkjptApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/swxxkjpt/SwxxkjptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.swxxkjpt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwxxkjptApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
